62c270df0c0141b57a54bd2710de7ba1728da6e5
manu-prakash-choudhary <manu.prakash2020@vitbhopal.ac.in>
2024-02-23 14:04:04 +0530

    Refactor findLCA function to use more descriptive variable names

--- trees/lowest_common_ancestor.py
+++ trees/lowest_common_ancestor.py
@@ -4,15 +4,15 @@ class Node:
         self.left = None 
         self.right = None
 
-def findLCA(root, n1, n2):
-    if root is None:
+def findLCA(curr_node, n1, n2):
+    if curr_node is None:
         return None
-    if root.value == n1 or root.value == n2:
-        return root
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
+    if curr_node.value == n1 or curr_node.value == n2:
+        return curr_node
+    left_lca = findLCA(curr_node.left, n1, n2)
+    right_lca = findLCA(curr_node.right, n1, n2)
     if left_lca and right_lca:
-        return root
+        return curr_node
     return left_lca if left_lca is not None else right_lca
 
 root = Node(20)
