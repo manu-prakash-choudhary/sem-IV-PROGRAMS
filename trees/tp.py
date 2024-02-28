f750e8a7cffb082d73032180d8ea147fa7cfc9d7
manu-prakash-choudhary <manu.prakash2020@vitbhopal.ac.in>
2024-02-28 15:43:40 +0530

    asd

--- trees/tp.py
+++ trees/tp.py
@@ -1,7 +1,7 @@
-with open ('run.sh', 'w') as rsh:
-    rsh.write('''\
-#! /bin/bash
-echo "I ran this"
-echo "more lines"
-python3 tp2.py $1
-''')
+# with open ('run.sh', 'w') as rsh:
+#     rsh.write('''\
+# #! /bin/bash
+# echo "I ran this"
+# echo "more lines"
+# python3 tp2.py $1
+# ''')
